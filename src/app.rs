da0af55812ab8ff27dff91b526c7ecf7eb59e0ca
mara <mara@the-witch.ru>
2024-02-28 04:31:59 +0300

    args

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
-use regex::{self, Regex};
 use chrono::{prelude::*, DateTime, Duration, Local, Utc};
+use regex::{self, Regex};
 
 pub struct App {
     pub from: DateTime<Local>,
@@ -10,7 +10,7 @@ impl App {
     pub fn new(first_arg: String) -> App {
         let mut app = App {
             from: Local::now(),
-            to: Local::now()
+            to: Local::now(),
         };
 
         let time = app.parse_time_argument(first_arg);
@@ -21,31 +21,25 @@ impl App {
     }
 
     fn parse_time_argument(&mut self, time_string: String) -> i64 {
-        let result: Result<i64, _> = time_string.parse();
-        match result {
-            Ok(_parsed_number) => {}
-            Err(_) => {
-                result = self.convert_arg_to_ms(time_string);
-                panic!("Failed to parse the string as an i64");
-            }
-        }
-        result.unwrap() * 1000
+        let re = Regex::new(r"(\d+)([^0-9]+)").unwrap();
+        let error: &str = "time argument must be NUMBER[s|m|h] format";
 
-    }
-
-    fn convert_arg_to_ms(&mut self, time_string: String) -> Option<i64> {
-        let re = Regex::new(r"(\d+)([hms])").unwrap();
         if let Some(captures) = re.captures(&time_string) {
             let value: i64 = captures[1].parse().unwrap();
             match &captures[2] {
-                "s" => Some(value * 1000),
-                "m" => Some(value * 1000 * 60),
-                "h" => Some(value * 1000 * 60 * 60),
-                _ => None, // todo
+                "s" => value * 1000,
+                "m" => value * 1000 * 60,
+                "h" => value * 1000 * 60 * 60,
+                _ => panic!("{}", error),
             }
         } else {
-            // todo
-            None
+            let result: Result<i64, _> = time_string.parse();
+            match result {
+                Ok(parsed_number) => parsed_number * 1000,
+                Err(_) => {
+                    panic!("{}", error)
+                }
+            }
         }
     }
 
